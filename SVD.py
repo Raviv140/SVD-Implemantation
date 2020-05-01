a4e3c17697b620c0eee202b2224f89d8aa152c46
Raviv Herrera <59799288+Raviv140@users.noreply.github.com>
2020-05-01 18:06:51 +0300

    Update SVD.py

--- SVD.py
+++ SVD.py
@@ -1,40 +1,35 @@
 import numpy as np
-import sympy as sy
+
 # Created by Raviv Herrera 
-#Three examples to try : 
-# A = np.array([[1, 1], [0, 1], [-1, 1]])
-# A = np.array([[1, 0, 1], [0, 1, 0], [0, 1, 1], [0, 1, 0], [1, 1, 0]])
-A = np.array([[1, 1, 0], [1, 0, 1], [0, 1, 1]])
-
-
-def svd_Implem(A):
+#A = np.array([[4, 1, 3], [8, 3, -2]])
+#A = np.array([[1, 1], [0, 1], [-1, 1]])
+#A = np.array([[2, 2], [-1, 1]])
+A = np.array([[122, 534, 2], [1, 640, 9], [4, 1, 1]])
+def mySVD(A):
     if type(A) != np.ndarray:
-        raise Exception("Must be a np.ndarray type!!")
-    A_trans = A.transpose()
-    singular_values = np.sort(np.linalg.eigvals(np.dot(A_trans, A)))[::-1] ** 0.5
-    Sigma = np.zeros((A.shape[0], A.shape[1]))
-    k = 0
-    for i in range(Sigma.shape[0]):
-        for j in range(Sigma.shape[1]):
-            if i == j:
-                Sigma[i, j] = singular_values[k]
-                k += 1
+        Exception("Data must be a np.ndarray type")
+    else:
+        S = np.zeros((A.shape[0], A.shape[1]))
+        singular_values, v = np.linalg.eig(np.dot(A.transpose(), A))
+        singular_values_sorted = np.sort(singular_values)[::-1] ** 0.5
+        for i in range(A.shape[0]):
+            for j in range(A.shape[1]):
+                if i == j:
+                    S[i, j] = singular_values_sorted[i]
 
-    V_trans = np.linalg.eig(Sigma[:len(singular_values), :])[1][::-1]
-    U = np.zeros((A.shape[0], A.shape[0]))
-    for i in range(len(singular_values)):
-        U[:, i] = 1 / singular_values[i] * np.dot(A, V_trans[:, i])
+        V = v[:, singular_values.argsort()[::-1]]
+        su, u = np.linalg.eig(np.dot(A, A.transpose()))
+        U = u[:, su.argsort()[::-1]]
 
-    u_rest = np.asarray(sy.Matrix(A_trans).nullspace())
-    for i in range(A.shape[0] - len(singular_values)):
-        temp = np.sum(u_rest[i] ** 2) ** 0.5
-        U[:, len(singular_values) + i] = 1 / temp * u_rest[i]
-
-    return U, Sigma, V_trans
+        return U, S, V.T
 
 
+U, S, V = mySVD(A)
+print("mySVD U is : \n", np.round(U, 3), f"\n\n mySVD {chr(931)} is : \n", np.round(S, 3), "\n\n mySVD V' is : \n",
+      np.round(V, 3), "\n")
+u1, s1, v1 = np.linalg.svd(A)
+print("NUMPY U is : \n", np.round(u1, 3), f"\n\n NUMPY {chr(931)} is : \n", np.round(s1, 3), "\n\n NUMPY V' is : \n",
+      np.round(v1, 3))
+"\n\n mySVD V' is : \n", np.round(V.T, 3)
 #Let us try the results : 
-
-U, Sigma, V_trans = svd_Implem(A)
-ans = np.mat(U) * np.mat(Sigma) * np.mat(V_trans)
-print(ans)
+print("The Matrix A is : \n", np.round(np.dot(U, S).dot(V), 3))
